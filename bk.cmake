fc0208b2b0327f9c2363d31bf71def6ef24aeeac
BenKoehler <ben.koehler@web.de>
2022-10-16 10:48:07 +0200

    replace CMAKE_CURRENT_SOURCE_DIR with PROJECT_SOURCE_DIR

--- bk.cmake
+++ bk.cmake
@@ -1,3 +1,3 @@
 function(link_rapidxml target MOD)
-    target_include_directories(${target} SYSTEM ${MOD} ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/rapidxml)
+    target_include_directories(${target} SYSTEM ${MOD} ${PROJECT_SOURCE_DIR}/thirdparty/rapidxml)
 endfunction()
